5d12bc98db9cbf23d9046a8240c30db1745ae026
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-01-19 12:36:45 +0530

    code done

--- Tree/Binary_Search_Tree.c
+++ Tree/Binary_Search_Tree.c
@@ -44,6 +44,36 @@ struct node *findMin(struct node *root){
     return root;
 }
 
+struct node *deleteNode(struct node *root,int data){
+    if(root == NULL){
+        return root;
+    }
+    if(data<root->data){
+        root->left = deleteNode(root->left,data);
+    }
+    else if(data>root->data){
+        root->right = deleteNode(root->right,data);
+    }
+    else{
+        //node with only one child 
+        if(root->left == NULL){
+            struct node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if(root->right = NULL){
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        //node with two children : replace the root with its inorder successor(smallest node in its right subtree)
+        struct node *temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right,temp->data);
+    }
+    return root;
+}
+
 int main(){
     struct node *root = NULL;
     int choice,data;
